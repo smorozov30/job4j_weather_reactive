8f374d20e010176d8c2b9260ce7affba0962c690
Sergey Morozov <moroz.future@gmail.com>
2021-02-07 22:45:34 +0400

    Spring boot Webflux [#428619]

---
+++ src/main/java/ru/job4j/weather/controller/WeatherControl.java
@@ -0,0 +1,47 @@
+package ru.job4j.weather.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import ru.job4j.weather.service.WeatherService;
+import ru.job4j.weather.model.Weather;
+
+import java.time.Duration;
+
+@RestController
+public class WeatherControl {
+
+    @Autowired
+    private final WeatherService weathers;
+
+    public WeatherControl(WeatherService weathers) {
+        this.weathers = weathers;
+    }
+
+    @GetMapping(value = "/all", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Weather> all() {
+        Flux<Weather> data = weathers.all();
+        Flux<Long> delay = Flux.interval(Duration.ofSeconds(3));
+        return Flux.zip(data, delay).map(Tuple2::getT1);
+    }
+
+    @GetMapping(value = "/get/{id}")
+    public Mono<Weather> getById(@PathVariable Integer id) {
+        return weathers.findById(id);
+    }
+
+    @GetMapping(value = "/hottest")
+    public Mono<Weather> getHottest() {
+        return weathers.findHottest();
+    }
+
+    @GetMapping(value = "/hotterThan/{t}")
+    public Flux<Weather> getHotterThan(@PathVariable Integer t) {
+        return weathers.findAllHotterThan(t);
+    }
+}
---
+++ src/main/java/ru/job4j/weather/service/WeatherService.java
@@ -0,0 +1,46 @@
+package ru.job4j.weather.service;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import ru.job4j.weather.model.Weather;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+@Service
+public class WeatherService {
+
+    private final Map<Integer, Weather> weathers = new ConcurrentHashMap<>();
+
+    {
+        weathers.put(1, new Weather(1, "Msc", 15));
+        weathers.put(2, new Weather(2, "SPb", 30));
+        weathers.put(3, new Weather(3, "Bryansk", 15));
+        weathers.put(4, new Weather(4, "Smolensk", 20));
+        weathers.put(5, new Weather(5, "Kiev", 45));
+        weathers.put(6, new Weather(6, "Minsk", 15));
+    }
+
+    public Mono<Weather> findById(Integer id) {
+        return Mono.justOrEmpty(weathers.get(id));
+    }
+
+    public Flux<Weather> all() {
+        return Flux.fromIterable(weathers.values());
+    }
+
+    public Mono<Weather> findHottest() {
+        return Mono.justOrEmpty(
+                weathers.values().stream().max(Comparator.comparingInt(Weather::getTemperature)).get()
+        );
+    }
+
+    public Flux<Weather> findAllHotterThan(Integer t) {
+        return Flux.fromIterable(
+                weathers.values().stream().filter(weather -> weather.getTemperature() > t).collect(Collectors.toList())
+        );
+    }
+}
